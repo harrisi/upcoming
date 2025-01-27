4601f9f06f0f4476939f2a73ec86108e61105d54
Ian Harris <ian@fofgof.xyz>
2025-01-27 13:41:22 -0800

    go back to jason
    fly's little web deploy thing uses elixir 1.16, so the JSON module from
    1.18 isn't available.
    
    I could specify my elixir version, but I don't want to yet.

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule Upcoming.MixProject do
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.26"},
-      # {:jason, "~> 1.2"},
+      {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
       {:httpoison, "~> 2.2"}
