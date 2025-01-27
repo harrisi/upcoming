b7e0dd63e426bac10a97bd5037699b8eb6c7f9c6
Ian Harris <ian@fofgof.xyz>
2025-01-27 13:35:55 -0800

    ephemeral cache for groups

--- lib/upcoming_web/controllers/upcoming_controller.ex
+++ lib/upcoming_web/controllers/upcoming_controller.ex
@@ -5,42 +5,78 @@ defmodule UpcomingWeb.UpcomingController do
   def index(conn, %{"group" => group} = params) do
     only_url = Map.get(params, "url")
 
-    with {:ok, res} <- HTTPoison.get("https://api.meetup.com/#{group}"),
-         {:ok, body} <- JSON.decode(res.body),
-         {:ok, next_event} <- Map.fetch(body, "next_event") do
-      # these should all be here, I think.
-      %{
-        "id" => id,
-        "time" => time,
-        "utc_offset" => utc_offset
-      } = next_event
+    # this is kinda messy, but, you know.
+    pt =
+      case :persistent_term.get(group, nil) do
+        nil ->
+          Logger.info("group not cached")
+          nil
 
-      dt =
-        DateTime.from_unix!(div(time, 1_000))
-        |> DateTime.shift(second: div(utc_offset, 1_000))
-
-      Logger.info("next event at #{dt}")
-      url = "https://www.meetup.com/#{group}/events/#{id}"
-
-      if only_url do
-        json(conn, %{url: url})
-      else
-        redirect(conn, external: url)
+        cache = {_url, event_time} ->
+          if DateTime.after?(DateTime.now!("Etc/UTC"), event_time) do
+            Logger.info("previously cached event passed")
+            nil
+          else
+            Logger.info("hitting cache for group")
+            cache
+          end
       end
-    else
-      _ ->
-        Logger.error("some error")
-        url = "https://www.meetup.com/#{group}/events"
 
-        if only_url do
-          json(conn, %{error: true, url: url})
-        else
-          redirect(conn, external: url)
-        end
+    url =
+      if pt == nil do
+        {url, dt} = do_fetch!(group)
+
+        :persistent_term.put(group, {url, dt})
+
+        url
+      else
+        {url, _dt} = pt
+
+        url
+      end
+
+    if only_url do
+      json(conn, %{url: url})
+    else
+      redirect(conn, external: url)
     end
   end
 
   def index(conn, _params) do
     json(conn, %{error: true, message: "must supply group name"})
+  end
+
+  defp do_fetch!(group) do
+    case do_fetch(group) do
+      {:ok, res} -> res
+      {:error, err} -> raise err
+    end
+  end
+
+  defp do_fetch(group) do
+    with {:ok, res} <- HTTPoison.get("https://api.meetup.com/#{group}"),
+         {:ok, body} <- JSON.decode(res.body),
+         {:ok, next_event} <- Map.fetch(body, "next_event") do
+      %{
+        "id" => id,
+        "time" => time
+        # I may want to expose this information at some point, but for now I
+        # don't care
+        # "utc_offset" => utc_offset
+      } = next_event
+
+      dt =
+        DateTime.from_unix!(div(time, 1_000))
+
+      # for if/when I want to expose this
+      # |> DateTime.shift(second: div(utc_offset, 1_000))
+
+      Logger.info("next event at #{dt}")
+      {:ok, {"https://www.meetup.com/#{group}/events/#{id}", dt}}
+    else
+      _ ->
+        Logger.error("some error")
+        {:error, "https://www.meetup.com/#{group}/events"}
+    end
   end
 end
